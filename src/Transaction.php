0f45599bd5b30a9a79497ab5957d882b7acf5c8f
Gawain Lynch <gawain.lynch@gmail.com>
2016-03-07 09:46:21 +0100

    Payment transaction object class

---
+++ src/Transaction.php
@@ -0,0 +1,276 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Payments;
+
+/**
+ * Payment transaction object class.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Transaction
+{
+    /** @var float */
+    protected $amount;
+    /** @var string */
+    protected $currency;
+    /** @var string */
+    protected $description;
+    /** @var string */
+    protected $transactionId;
+    /** @var string */
+    protected $transactionReference;
+    /** @var string */
+    protected $cardReference;
+    /** @var string */
+    protected $returnUrl;
+    /** @var string */
+    protected $cancelUrl;
+    /** @var string */
+    protected $notifyUrl;
+    /** @var string */
+    protected $issuer;
+
+    /**
+     * Create a new instance.
+     *
+     * @param float  $amount
+     * @param string $currency
+     * @param string $description
+     * @param string $transactionId
+     * @param string $transactionReference
+     * @param string $cardReference
+     * @param string $returnUrl
+     * @param string $cancelUrl
+     * @param string $notifyUrl
+     * @param string $issuer
+     *
+     * @return Transaction
+     */
+    public static function create(
+        $amount,
+        $currency,
+        $description,
+        $transactionId,
+        $transactionReference,
+        $cardReference,
+        $returnUrl,
+        $cancelUrl,
+        $notifyUrl,
+        $issuer
+    )
+    {
+        $class = new self();
+        $class->setAmount($amount);
+        $class->setCurrency($currency);
+        $class->setDescription($description);
+        $class->setTransactionId($transactionId);
+        $class->setTransactionReference($transactionReference);
+        $class->setCardReference($cardReference);
+        $class->setReturnUrl($returnUrl);
+        $class->setCancelUrl($cancelUrl);
+        $class->setNotifyUrl($notifyUrl);
+        $class->setIssuer($issuer);
+
+        return $class;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIssuer()
+    {
+        return $this->issuer;
+    }
+
+    /**
+     * @param string $issuer
+     *
+     * @return Transaction
+     */
+    public function setIssuer($issuer)
+    {
+        $this->issuer = $issuer;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param float $amount
+     *
+     * @return Transaction
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param string $currency
+     *
+     * @return Transaction
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     *
+     * @return Transaction
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTransactionId()
+    {
+        return $this->transactionId;
+    }
+
+    /**
+     * @param string $transactionId
+     *
+     * @return Transaction
+     */
+    public function setTransactionId($transactionId)
+    {
+        $this->transactionId = $transactionId;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTransactionReference()
+    {
+        return $this->transactionReference;
+    }
+
+    /**
+     * @param string $transactionReference
+     *
+     * @return Transaction
+     */
+    public function setTransactionReference($transactionReference)
+    {
+        $this->transactionReference = $transactionReference;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCardReference()
+    {
+        return $this->cardReference;
+    }
+
+    /**
+     * @param string $cardReference
+     *
+     * @return Transaction
+     */
+    public function setCardReference($cardReference)
+    {
+        $this->cardReference = $cardReference;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReturnUrl()
+    {
+        return $this->returnUrl;
+    }
+
+    /**
+     * @param string $returnUrl
+     *
+     * @return Transaction
+     */
+    public function setReturnUrl($returnUrl)
+    {
+        $this->returnUrl = $returnUrl;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCancelUrl()
+    {
+        return $this->cancelUrl;
+    }
+
+    /**
+     * @param string $cancelUrl
+     *
+     * @return Transaction
+     */
+    public function setCancelUrl($cancelUrl)
+    {
+        $this->cancelUrl = $cancelUrl;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNotifyUrl()
+    {
+        return $this->notifyUrl;
+    }
+
+    /**
+     * @param string $notifyUrl
+     *
+     * @return Transaction
+     */
+    public function setNotifyUrl($notifyUrl)
+    {
+        $this->notifyUrl = $notifyUrl;
+
+        return $this;
+    }
+}
