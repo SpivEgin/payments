ed315f2a471401a43a187ffc86dd1f18b243d977
Gawain Lynch <gawain.lynch@gmail.com>
2016-03-07 11:46:58 +0100

    Payment entity class

---
+++ src/Storage/Entity/Payment.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Payments\Storage\Entity;
+
+/**
+ * Payment entity class.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Payment
+{
+    /** @var string */
+    protected $customerId;
+    /** @var string */
+    protected $gateway;
+    /** @var string */
+    protected $transactionId;
+    /** @var string */
+    protected $transactionReference;
+    /** @var float */
+    protected $amount;
+    /** @var string */
+    protected $currency;
+    /** @var string */
+    protected $description;
+
+    /**
+     * @return string
+     */
+    public function getCustomerId()
+    {
+        return $this->customerId;
+    }
+
+    /**
+     * @param string $customerId
+     */
+    public function setCustomerId($customerId)
+    {
+        $this->customerId = $customerId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGateway()
+    {
+        return $this->gateway;
+    }
+
+    /**
+     * @param string $gateway
+     */
+    public function setGateway($gateway)
+    {
+        $this->gateway = $gateway;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTransactionId()
+    {
+        return $this->transactionId;
+    }
+
+    /**
+     * @param string $transactionId
+     */
+    public function setTransactionId($transactionId)
+    {
+        $this->transactionId = $transactionId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTransactionReference()
+    {
+        return $this->transactionReference;
+    }
+
+    /**
+     * @param string $transactionReference
+     */
+    public function setTransactionReference($transactionReference)
+    {
+        $this->transactionReference = $transactionReference;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param float $amount
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param string $currency
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+}
