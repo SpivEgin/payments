a6faa0cd914a7b9898969f6d3e77f95df8ebf2a8
Gawain Lynch <gawain.lynch@gmail.com>
2016-05-04 16:22:04 +0200

    Consistent naming

--- src/Event/PaymentEvents.php
+++ src/Event/PaymentEvents.php
@@ -30,14 +30,14 @@ class PaymentEvents
     const PAYMENT_DELETE_FAILURE = 'payment.delete.failure';
 
     const PAYMENT_PURCHASE_INITIATE = 'payment.purchase.initiate';
-    const PAYMENT_PURCHASE_CANCELLED = 'payment.purchase.cancelled';
+    const PAYMENT_PURCHASE_CANCEL = 'payment.purchase.cancel';
     const PAYMENT_PURCHASE_SUCCESS = 'payment.purchase.success';
     const PAYMENT_PURCHASE_FAILURE = 'payment.purchase.failure';
 
     const PAYMENT_UPDATE_INITIATE = 'payment.update.initiate';
     const PAYMENT_UPDATE_SUCCESS = 'payment.update.success';
     const PAYMENT_UPDATE_FAILURE = 'payment.update.failure';
-    
+
     /**
      * Constructor.
      */
